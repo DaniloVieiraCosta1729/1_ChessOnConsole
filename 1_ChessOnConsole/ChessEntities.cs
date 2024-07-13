bd61890a6389e31546ee16f76e4bd14bc3e610b5
Danilo Vieria Costa <danilo.11235@outlook.com>
2024-07-13 14:33:24 -0300

    Adicionar arquivos de projeto.

---
+++ 1_ChessOnConsole/ChessEntities.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ChessOnConsole
+{
+    public class ChessEntities
+    {
+        public string tabuleiroFEN;
+
+        public ChessEntities()
+        {
+            tabuleiroFEN = "r1bq1rk1/ppp2ppp/2p2n2/2b5/2B1P3/2N5/PPPP1PPP/R1BQ1RK1 b - - 3 7";
+        }
+
+        public string[] geradorVetorFEN(string tabuleiro)
+        {
+            string[] vetorFEN = tabuleiro.Split('/', ' ');
+            return vetorFEN;
+        }
+
+        delegate string[] geradorVetor(string tabuleiro);        
+    }
+
+    // Definindo as peças com ASCII
+    public enum ChessPiece
+    {
+        vazio = ' ',
+
+        peaoPreto = 'p',
+        torrePreta = 'n',
+        cavaloPreto = 'n',
+        bispoPreto = 'b',
+        damaPreta = 'q',
+        reiPreto = 'k',
+
+        peaoBranco = 'P',
+        torreBranca = 'R',
+        cavaloBranco = 'N',
+        bispoBranco = 'B',
+        damaBranca = 'Q',
+        reiBranco = 'K'
+    }
+
+}
---
+++ 1_ChessOnConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using _1_ChessOnConsole;
+
+ChessEntities xadrez = new ChessEntities();
+
+string estado = xadrez.tabuleiroFEN;
+
+Console.WriteLine(estado);
+
+string[] vetor = xadrez.geradorVetorFEN(estado);
+
+Console.WriteLine(vetor.Length);
+Console.WriteLine(vetor[0]);
+Console.WriteLine(vetor[1]);
+Console.WriteLine(vetor[2]);
+Console.WriteLine(vetor[3]);
+Console.WriteLine(vetor[4]);
+Console.WriteLine(vetor[5]);
+Console.WriteLine(vetor[6]);
+Console.WriteLine(vetor[7]);
+Console.WriteLine(vetor[8]);
+Console.WriteLine(vetor[9]);
+Console.WriteLine(vetor[10]);
+Console.WriteLine(vetor[11]);
+Console.WriteLine(vetor[12]);
+
