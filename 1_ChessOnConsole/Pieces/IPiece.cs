52295eeabcb8b0e71fce6a29b815cd09f6bd978e
Danilo Vieria Costa <danilo.11235@outlook.com>
2024-10-27 11:51:01 -0300

    Refatora toda estrutura da aplicação.

---
+++ 1_ChessOnConsole/Pieces/IPiece.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ChessOnConsole.Pieces
+{
+    internal interface IPiece
+    {
+        public char Cor { get; set; }
+        public int[] CasaAtual { get; set; }
+
+        public string Move(int[] destino);
+        public string ValidaMove(int[] destino);
+
+    }
+}
---
+++ 1_ChessOnConsole/Pieces/Rook.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ChessOnConsole.Pieces
+{
+    internal class Rook : IPiece
+    {
+        private readonly char _tipo = 'r';
+
+        public char Cor { get; set; }
+        public int[] CasaAtual { get; set; }
+
+        public Rook(char cor, int[] casaAtual)
+        {
+            Cor = cor;
+            CasaAtual = casaAtual;
+        }
+
+        public string Move(int[] destino)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ValidaMove(int[] destino)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
